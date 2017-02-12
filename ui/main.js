70e6195f086746faac37cd66d6a0a19b46c8a5cb
Sowmiya Nagarajan <sowmiyanagarajan98@gmail.com>
2017-02-12 17:38:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+
 var element=document.getElementById('main-text');
 element.innerHTML="Just Changed :D";
+
+var ele=document.getElementById('image1');
+ele.onclick = function(){
+  ele.style.marginLeft='100px';  
+};
