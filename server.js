16c794cae5f0078b347966117ed23142246c75f2
Sowmiya Nagarajan <sowmiyanagarajan98@gmail.com>
2017-02-12 17:21:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get('/page', function (req, res) {
   res.send(page);
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
