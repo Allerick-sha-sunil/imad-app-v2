fa1bf35e61447733eebfa02b60d1c706b496f369
Sowmiya Nagarajan <sowmiyanagarajan98@gmail.com>
2017-02-13 22:57:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,7 @@
-console.log('Loaded!');
+var counter=0;
+var button= document.getElementById('counter');
+button.onclick = function(){
 
-var element=document.getElementById('main-text');
-element.innerHTML="Just Changed :D";
-
-var ele=document.getElementById('image1');
-var ml=0;
-function function1(){
-    ml=ml+5;
-    ele.style.marginLeft=ml+'px';
-}
-ele.onclick=function(){
-    var interval=setInterval(function1,50);
+    counter=counter+1;
+    var span=document.getElementById('count');
 };
